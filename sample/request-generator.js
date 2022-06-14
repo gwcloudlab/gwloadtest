ce27e73983e8aa3c3637a2be12269acdfd3543f9
Dania Abdalla <daniaabdalla@gwu.edu>
2022-06-14 00:00:19 -0400

    working commit

--- sample/request-generator.js
+++ sample/request-generator.js
@@ -1,37 +1,36 @@
-'use strict';
 
-/**
- * Sample request generator usage.
- * Contributed by jjohnsonvng:
- * https://github.com/alexfernandez/loadtest/issues/86#issuecomment-211579639
- */
-
+// requires
 const loadtest = require('../lib/loadtest.js');
+const testserver = require('../lib/testserver.js');
+//const configuration = require('../sample/config.json');   
+
+const fs = require('fs');
+
 
 const options = {
-	url: 'http://yourHost',
-	concurrency: 5,
-	method: 'POST',
-	body:'',
-	requestsPerSecond:5,
-	maxSeconds:30,
-	requestGenerator: (params, options, client, callback) => {
-		const message = '{"hi": "ho"}';
-		options.headers['Content-Length'] = message.length;
-		options.headers['Content-Type'] = 'application/json';
-		options.body = 'YourPostData';
-		options.path = 'YourURLPath';
-		const request = client(options, callback);
-		request.write(message);
-		return request;
-	}
+    url: 'https://www.google.com/',
+    statusCallback: statusCallback, 
+	requestsPerSecond: 5,
+	rpsInterval: 60
 };
 
-loadtest.loadTest(options, (error, results) => {
-	if (error) {
-		return console.error('Got an error: %s', error);
-	}
-	console.log(results);
-	console.log('Tests run successfully');
+
+function statusCallback(error, result, latency) {
+    console.log('----');
+    console.log('Request elapsed milliseconds: ', result.requestElapsed);
+ 	let f = result.requestElapsed.toString()+ '\n';
+   // fs.writeFile('test.txt', f, { flag: 'a+' }, err => {});
+    console.log('Request index: ', result.requestIndex);
+}
+
+loadtest.loadTest(options, function(error) {
+    if (error) {
+        return console.error('Got an error: %s', error);
+    }
+    console.log('Tests run successfully');
 });
+
+// exports
+exports.loadTest = loadtest.loadTest;
+exports.startServer = testserver.startServer;
 
