8d9bac5452c0e9cc64c63e1ce45067d200ec9f8c
Dania Abdalla <daniaabdalla@gwu.edu>
2022-07-05 10:45:48 -0400

    added timestamps, changed file type to csv, filenames include rps + interval

--- sample/request-generator.js
+++ sample/request-generator.js
@@ -5,22 +5,26 @@ const testserver = require('../lib/testserver.js');
 
 
 const fs = require('fs');
-
+rps = 10;
+rpsInter= 10;
+let fileTitle = "sample/log-rps-"+rps+"-iv-"+rpsInter+ ".csv";
 
 const options = {
-    url: 'https://www.google.com/',
+    url: 'https://www.google.com',
     statusCallback: statusCallback, 
-	requestsPerSecond: 100,
-	rpsInterval: 11
+	requestsPerSecond: rps,
+	rpsInterval: rpsInter
 };
 
 
 function statusCallback(error, result, latency) {
     console.log('----');
-    console.log('Request elapsed milliseconds: ', result.requestElapsed);
- 	let f = result.requestElapsed.toString()+ '\n';
-   // fs.writeFile('test.txt', f, { flag: 'a+' }, err => {});
+    console.log('Timestamp: ', result.startTime.toFixed(2));
     console.log('Request index: ', result.requestIndex);
+    console.log('Request elapsed milliseconds: ', result.requestElapsed);
+    let n = result.requestElapsed.toFixed();
+ 	let s = result.requestIndex + ", " +result.startTime.toFixed(2) + ", " + n.toString() + ", " + result.statusCode + "\n";
+    fs.writeFile(fileTitle, s, { flag: 'a+' }, err => {});
 	console.log('Code: ', result.statusCode);
 }
 
